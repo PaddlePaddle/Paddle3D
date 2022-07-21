0a8290c677f16c3da3508f5f605fb2b808459d75
wuzewu <wuzewu@baidu.com>
2022-07-21 17:40:40 +0800

    Support custom jit

--- paddle3d/ops/setup.py
+++ paddle3d/ops/setup.py
@@ -1,43 +1,9 @@
 import paddle
 from paddle.utils.cpp_extension import CppExtension, CUDAExtension, setup
 
-custom_ops = [
-    # voxelize
-    {
-        'name': 'voxelize',
-        'sources': ['voxel/voxelize_op.cc', 'voxel/voxelize_op.cu']
-    },
-    # iou3d_nms
-    {
-        'name':
-        'iou3d_nms_cuda',
-        'sources': [
-            'iou3d_nms/iou3d_cpu.cpp', 'iou3d_nms/iou3d_nms_api.cpp',
-            'iou3d_nms/iou3d_nms.cpp', 'iou3d_nms/iou3d_nms_kernel.cu'
-        ]
-    },
-    # centerpoint_postprocess
-    {
-        'name':
-        'centerpoint_postprocess',
-        'sources': [
-            'centerpoint_postprocess/iou3d_nms_kernel.cu',
-            'centerpoint_postprocess/postprocess.cc',
-            'centerpoint_postprocess/postprocess.cu'
-        ]
-    },
-    # grid_sample_3d
-    {
-        'name':
-        'grid_sample_3d',
-        'sources': [
-            'grid_sample_3d/grid_sample_3d.cc',
-            'grid_sample_3d/grid_sample_3d.cu'
-        ]
-    }
-]
+from paddle3d.ops import custom_ops
 
-for op_dict in custom_ops:
+for op_name, op_dict in custom_ops.items():
     sources = op_dict.get('sources', [])
     flags = None
 
@@ -52,4 +18,4 @@ for op_dict in custom_ops:
         continue
 
     extension = extension(sources=sources)
-    setup(name=op_dict['name'], ext_modules=extension, extra_compile_args=flags)
+    setup(name=op_name, ext_modules=extension, extra_compile_args=flags)
