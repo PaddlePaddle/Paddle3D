cfeb32025f19a005448faded38b2608330b21945
wuzewu <wuzewu@baidu.com>
2022-08-01 10:12:20 +0800

    Support for building custom op just-in-time

--- paddle3d/ops/setup.py
+++ paddle3d/ops/setup.py
@@ -1,44 +1,10 @@
 import paddle
 from paddle.utils.cpp_extension import CppExtension, CUDAExtension, setup
 
-custom_ops = [
-    # voxelize
-    {
-        'name': 'voxelize',
-        'sources': ['voxel/voxelize_op.cc', 'voxel/voxelize_op.cu']
-    },
-    # iou3d_nms
-    {
-        'name':
-        'iou3d_nms_cuda',
-        'sources': [
-            'iou3d_nms/iou3d_cpu.cpp', 'iou3d_nms/iou3d_nms_api.cpp',
-            'iou3d_nms/iou3d_nms.cpp', 'iou3d_nms/iou3d_nms_kernel.cu'
-        ]
-    },
-    # centerpoint_postprocess
-    {
-        'name':
-        'centerpoint_postprocess',
-        'sources': [
-            'centerpoint_postprocess/iou3d_nms_kernel.cu',
-            'centerpoint_postprocess/postprocess.cc',
-            'centerpoint_postprocess/postprocess.cu'
-        ]
-    },
-    # grid_sample_3d
-    {
-        'name':
-        'grid_sample_3d',
-        'sources': [
-            'grid_sample_3d/grid_sample_3d.cc',
-            'grid_sample_3d/grid_sample_3d.cu'
-        ]
-    }
-]
+from paddle3d.ops import custom_ops
 
-for op_dict in custom_ops:
-    sources = op_dict.get('sources', [])
+for op_name, op_dict in custom_ops.items():
+    sources = op_dict.pop('sources', [])
     flags = None
 
     if paddle.device.is_compiled_with_cuda():
@@ -52,4 +18,8 @@ for op_dict in custom_ops:
         continue
 
     extension = extension(sources=sources)
-    setup(name=op_dict['name'], ext_modules=extension, extra_compile_args=flags)
+    setup(
+        name=op_name,
+        ext_modules=extension,
+        extra_compile_args=flags,
+        **op_dict)
