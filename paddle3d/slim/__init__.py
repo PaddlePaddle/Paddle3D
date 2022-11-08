2756206459f6a2a5a029de95cc788a17458a1e5d
Birdylx <Birdylx@users.noreply.github.com>
2022-11-08 12:12:35 +0000

    support quant

--- paddle3d/slim/__init__.py
+++ paddle3d/slim/__init__.py
@@ -14,28 +14,34 @@ def build_slim_model(cfg, slim_cfg_path):
 
     slim_type = slim_dic['slim_type']
     if slim_type == "QAT":
+        # create QAT
         quant_config = slim_dic["slim_config"]['quant_config']
         slim = QAT(quant_config=quant_config)
-        # load pretrain weight befor qat
+
+        # load pretrained weight befor qat
         load_pretrained_model(cfg.model, slim_dic["pretrain_weights"])
+
+        # slim the model
         slim(cfg.model)
+
+        # add a new filed 'slim' in cfg for export
         cfg.slim = {'slim_type': slim_type, 'slim_cls': slim}
     else:
-        raise ValueError("{} is not supported yet")
+        raise ValueError("slim method `{}` is not supported yet")
 
     # update finetune params
-    merge_dic(cfg.dic, slim_dic.get("finetune_config", {}))
+    update_dic(cfg.dic, slim_dic.get("finetune_config", {}))
 
     return cfg
 
 
-def merge_dic(dic, another_dic):
-    """ Recusive update dic by another_dic
+def update_dic(dic, another_dic):
+    """Recursive update dic by another_dic
     """
     for k, _ in another_dic.items():
         if (k in dic and isinstance(dic[k], dict)) and isinstance(
                 another_dic[k], dict):
-            merge_dic(dic[k], another_dic[k])
+            update_dic(dic[k], another_dic[k])
         else:
             dic[k] = another_dic[k]
     return dic
