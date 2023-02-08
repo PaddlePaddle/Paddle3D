5610f4bd30e91740202777f6cd94f7ceddbcfe22
Birdylx <29754889+Birdylx@users.noreply.github.com>
2023-02-08 03:12:24 +0000

    Reuse losses & lidar train check pass

--- paddle3d/models/detection/bevfusion/__init__.py
+++ paddle3d/models/detection/bevfusion/__init__.py
@@ -15,4 +15,3 @@
 from .bevf_faster_rcnn import *
 from .bevf_transforms import *
 from .mvx_faster_rcnn import *
-from .loss import *
--- paddle3d/models/losses/smooth_l1_loss.py
+++ paddle3d/models/losses/smooth_l1_loss.py
@@ -13,6 +13,10 @@
 # limitations under the License.
 
 import paddle
+import paddle.nn as nn
+
+from paddle3d.apis import manager
+from .utils import weight_reduce_loss
 
 
 def smooth_l1_loss(input, target, beta, reduction="none"):
@@ -50,3 +54,82 @@ def smooth_l1_loss(input, target, beta, reduction="none"):
     elif reduction == "sum":
         loss = loss.sum()
     return loss
+
+
+@manager.LOSSES.add_component
+class SmoothL1Loss(nn.Layer):
+    """Smooth L1 loss.
+
+    Args:
+        beta (float, optional): The threshold in the piecewise function.
+            Defaults to 1.0.
+        reduction (str, optional): The method to reduce the loss.
+            Options are "none", "mean" and "sum". Defaults to "mean".
+        loss_weight (float, optional): The weight of loss.
+    """
+
+    def __init__(self, beta=1.0, reduction='mean', loss_weight=1.0):
+        super(SmoothL1Loss, self).__init__()
+        self.beta = beta
+        self.reduction = reduction
+        self.loss_weight = loss_weight
+
+    def smooth_l1_loss(self,
+                       pred,
+                       target,
+                       weight,
+                       beta=1.0,
+                       reduction='none',
+                       avg_factor=None):
+        """Smooth L1 loss.
+
+        Args:
+            pred (paddle.Tensor): The prediction.
+            target (paddle.Tensor): The learning target of the prediction.
+            beta (float, optional): The threshold in the piecewise function.
+                Defaults to 1.0.
+
+        Returns:
+            paddle.Tensor: Calculated loss
+        """
+        assert beta > 0
+        assert pred.shape == target.shape and target.numel() > 0
+        diff_ = paddle.abs(pred - target)
+        loss = paddle.where(diff_ < beta, 0.5 * diff_ * diff_ / beta,
+                            diff_ - 0.5 * beta)
+        loss = weight_reduce_loss(loss, weight, reduction, avg_factor)
+        return loss
+
+    def forward(self,
+                pred,
+                target,
+                weight=None,
+                avg_factor=None,
+                reduction_override=None,
+                **kwargs):
+        """Forward function.
+
+        Args:
+            pred (paddle.Tensor): The prediction.
+            target (paddle.Tensor): The learning target of the prediction.
+            weight (paddle.Tensor, optional): The weight of loss for each
+                prediction. Defaults to None.
+            avg_factor (int, optional): Average factor that is used to average
+                the loss. Defaults to None.
+            reduction_override (str, optional): The reduction method used to
+                override the original reduction method of the loss.
+                Defaults to None.
+        """
+        assert reduction_override in (None, 'none', 'mean', 'sum')
+        reduction = (reduction_override
+                     if reduction_override else self.reduction)
+
+        loss_bbox = self.loss_weight * self.smooth_l1_loss(
+            pred,
+            target,
+            weight,
+            beta=self.beta,
+            reduction=reduction,
+            avg_factor=avg_factor,
+            **kwargs)
+        return loss_bbox
