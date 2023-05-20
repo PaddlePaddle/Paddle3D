c2a428c30987590e54ca942a4b8cfeaa93cd182f
ZS-YANG <1677926753@qq.com>
2023-05-20 17:52:26 +0800

    add c++ radial_n_tangential_undistort

--- contrib/PaddleRendering/pprndr/cpp_extensions/radial_n_tangential_undistort/setup.py
+++ contrib/PaddleRendering/pprndr/cpp_extensions/radial_n_tangential_undistort/setup.py
@@ -1,10 +1,12 @@
-from paddle.utils.cpp_extension import CUDAExtension, setup, CppExtension
 import paddle
+from paddle.utils.cpp_extension import CppExtension, CUDAExtension, setup
 
-if paddle.fluid.is_compiled_with_cuda() is True:
+if paddle.device.is_compiled_with_cuda():
     setup(name='my_undist',
+          version='1.0.0',
           ext_modules=CUDAExtension(
               sources=['./src/undist_gpu.cc', './src/undist_gpu.cu']))
 else:
     setup(name='my_undist',
+          version='1.0.0',
           ext_modules=CppExtension(sources=['./src/undist_cpu.cc']))
