e7588a065601fa567e15666b2d197e3e67676a41
ZS-YANG <52490943+ZS-YANG@users.noreply.github.com>
2023-05-22 13:59:35 +0800

    Update setup.py

--- contrib/PaddleRendering/pprndr/cpp_extensions/radial_n_tangential_undistort/setup.py
+++ contrib/PaddleRendering/pprndr/cpp_extensions/radial_n_tangential_undistort/setup.py
@@ -3,12 +3,12 @@ from paddle.utils.cpp_extension import CppExtension, CUDAExtension, setup
 
 if paddle.device.is_compiled_with_cuda():
     setup(
-        name='my_undist',
+        name='undistort',
         version='1.0.0',
         ext_modules=CUDAExtension(
             sources=['./src/undist_gpu.cc', './src/undist_gpu.cu']))
 else:
     setup(
-        name='my_undist',
+        name='undistort',
         version='1.0.0',
         ext_modules=CppExtension(sources=['./src/undist_cpu.cc']))
