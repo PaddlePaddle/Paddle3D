43467e16e7f63120472de9694cf957af8f30bdc3
wangna11BD <wangna11@baidu.com>
2023-06-12 06:25:16 +0000

    fix coda style

--- paddle3d/apis/trainer.py
+++ paddle3d/apis/trainer.py
@@ -49,10 +49,11 @@ def default_dataloader_build_fn(**kwargs) -> paddle.io.DataLoader:
             # Do eval in single device
             BatchSampler = paddle.io.BatchSampler
 
-        batch_sampler = BatchSampler(dataset,
-                                     batch_size=batch_size,
-                                     shuffle=shuffle,
-                                     drop_last=drop_last)
+        batch_sampler = BatchSampler(
+            dataset,
+            batch_size=batch_size,
+            shuffle=shuffle,
+            drop_last=drop_last)
 
         if hasattr(model, 'collate_fn'):
             collate_fn = model.collate_fn
@@ -70,11 +71,12 @@ def default_dataloader_build_fn(**kwargs) -> paddle.io.DataLoader:
                                "disable shared_memory in DataLoader")
                 use_shared_memory = False
 
-        return paddle.io.DataLoader(dataset=dataset,
-                                    batch_sampler=batch_sampler,
-                                    collate_fn=collate_fn,
-                                    use_shared_memory=use_shared_memory,
-                                    **args)
+        return paddle.io.DataLoader(
+            dataset=dataset,
+            batch_sampler=batch_sampler,
+            collate_fn=collate_fn,
+            use_shared_memory=use_shared_memory,
+            **args)
 
     return _generate_loader
 
@@ -234,8 +236,8 @@ class Trainer:
                 "Attempt to restore parameters from an empty checkpoint")
 
         if env.local_rank == 0:
-            self.log_writer = LogWriter(logdir=self.checkpoint.rootdir,
-                                        file_name=vdl_file_name)
+            self.log_writer = LogWriter(
+                logdir=self.checkpoint.rootdir, file_name=vdl_file_name)
             self.checkpoint.record('vdl_file_name',
                                    os.path.basename(self.log_writer.file_name))
             self.checkpoint.record('train_by_epoch', self.train_by_epoch)
@@ -265,12 +267,13 @@ class Trainer:
             cycle_epoch = ema_cfg.get('cycle_epoch', -1)
             ema_black_list = ema_cfg.get('ema_black_list', None)
             ema_filter_no_grad = ema_cfg.get('ema_filter_no_grad', False)
-            self.ema = ModelEMA(self.model,
-                                decay=ema_decay,
-                                ema_decay_type=ema_decay_type,
-                                cycle_epoch=cycle_epoch,
-                                ema_black_list=ema_black_list,
-                                ema_filter_no_grad=ema_filter_no_grad)
+            self.ema = ModelEMA(
+                self.model,
+                decay=ema_decay,
+                ema_decay_type=ema_decay_type,
+                cycle_epoch=cycle_epoch,
+                ema_black_list=ema_black_list,
+                ema_filter_no_grad=ema_filter_no_grad)
 
     def train(self):
         """
@@ -346,16 +349,16 @@ class Trainer:
 
                 lr = self.optimizer.get_lr()
 
-                output = training_step(model,
-                                       self.optimizer,
-                                       sample,
-                                       self.cur_iter,
-                                       scaler=self.scaler,
-                                       amp_cfg=self.amp_cfg,
-                                       all_fused_tensors=getattr(
-                                           self.optimizer, 'all_fused_tensors',
-                                           None),
-                                       group=group)
+                output = training_step(
+                    model,
+                    self.optimizer,
+                    sample,
+                    self.cur_iter,
+                    scaler=self.scaler,
+                    amp_cfg=self.amp_cfg,
+                    all_fused_tensors=getattr(self.optimizer,
+                                              'all_fused_tensors', None),
+                    group=group)
 
                 for loss_name, loss_value in output.items():
                     losses_sum[loss_name] += float(loss_value)
@@ -368,13 +371,15 @@ class Trainer:
                     for loss_name, loss_value in losses_sum.items():
                         loss_value = loss_value / self.scheduler.log_interval
                         loss_log += ', {}={:.6f}'.format(loss_name, loss_value)
-                        self.log_writer.add_scalar(tag='Training/' + loss_name,
-                                                   value=loss_value,
-                                                   step=self.cur_iter)
+                        self.log_writer.add_scalar(
+                            tag='Training/' + loss_name,
+                            value=loss_value,
+                            step=self.cur_iter)
 
-                    self.log_writer.add_scalar(tag='Training/learning_rate',
-                                               value=lr,
-                                               step=self.cur_iter)
+                    self.log_writer.add_scalar(
+                        tag='Training/learning_rate',
+                        value=lr,
+                        step=self.cur_iter)
 
                     self.logger.info(
                         '[TRAIN] epoch={}/{}, iter={}/{} {}, lr={:.6f}, batch_cost: {:.6f} sec, ips: {:.6f} images/s | ETA {}'
