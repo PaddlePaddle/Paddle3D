867fdf78c207de6583b256f7f2b821bcaee50c8b
USTCKAY <1136780339@qq.com>
2023-08-25 00:45:05 +0800

    [TIPC]update xpu and npu tipc scripts

--- test_tipc/test_train_inference_python_npu.sh
+++ test_tipc/test_train_inference_python_npu.sh
@@ -28,6 +28,9 @@ dataline=`cat $FILENAME`
 sed -i 's/\"gpu\"/\"npu\"/g' test_tipc/test_train_inference_python.sh
 sed -i 's/--gpus/--npus/g' test_tipc/test_train_inference_python.sh
 
+# python has been updated to version 3.9 for npu backend
+sed -i "s/python3.7/python3.9/g" $FILENAME
+
 # parser params
 IFS=$'\n'
 lines=(${dataline})
--- test_tipc/test_train_inference_python_xpu.sh
+++ test_tipc/test_train_inference_python_xpu.sh
@@ -28,6 +28,9 @@ dataline=`cat $FILENAME`
 sed -i 's/\"gpu\"/\"xpu\"/g' test_tipc/test_train_inference_python.sh
 sed -i 's/--gpus/--xpus/g' test_tipc/test_train_inference_python.sh
 
+# python has been updated to version 3.9 for npu backend
+sed -i "s/python3.7/python3.9/g" $FILENAME
+
 # parser params
 IFS=$'\n'
 lines=(${dataline})
